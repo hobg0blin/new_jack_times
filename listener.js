287f697d47cc63376968ff061f9a417338668243
Brent Bailey <brent@mouth.com>
2017-02-09 16:56:00 -0500

    add url conditional to storage

--- listener.js
+++ listener.js
@@ -1,19 +1,22 @@
 var t = '';
 var airHorn = new Audio();
 var positionStore = [];
-chrome.storage.local.get(["positions"], (result) => {
-   positionStore = result['positions'] ? result['positions'] : [];
+console.log(window.location.href);
+chrome.storage.local.get([window.location.href], (result) => {
+   positionStore = result[window.location.href] ? result[window.location.href]['positions'] : [];
 });
 airHorn.src = chrome.runtime.getURL('air-horn-club-sample_1.mp3');
+//TODO: make site-specific and eventually shareable, highlight the airhorned text
 //for testing purposes, clear cache on pageload
 // chrome.storage.local.clear();
 function gText(e) {
     t = (document.all) ? document.selection.createRange().text : document.getSelection();
-    chrome.storage.local.get(["positions"], (result) => {
-      var array = result['positions']?result['positions']:[];
+    chrome.storage.local.get([window.location.href], (result) => {
+      var array = result[window.location.href]?result[window.location.href]['positions']:[];
       array.unshift(document.body.scrollTop);
       var positions = {};
-      positions['positions'] = array;
+      positions[window.location.href] = {};
+      positions[window.location.href]['positions'] = array;
       positionStore = array;
       chrome.storage.local.set(positions);
     })
